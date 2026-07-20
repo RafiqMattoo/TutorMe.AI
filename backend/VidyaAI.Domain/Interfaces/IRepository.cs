d4ba4dbcf667fb1b0c9e060845168ba5a19ab808
sheikhahmadfirdous <sheikhfirdous655@gmail.com>
2026-07-20 12:48:21 +0530

    folder structure added

--- backend/VidyaAI.Domain/Interfaces/IRepository.cs
+++ backend/VidyaAI.Domain/Interfaces/IRepository.cs
@@ -13,24 +13,6 @@ public interface IRepository<T> where T : BaseEntity
     Task DeleteAsync(T entity, CancellationToken ct = default);
 }
 
-public interface ISchoolRepository : IRepository<School>
-{
-    Task<bool> EmailExistsAsync(string email, CancellationToken ct = default);
-}
-
-public interface IUserRepository : IRepository<User>
-{
-    Task<User?> GetByEmailAsync(string email, CancellationToken ct = default);
-    Task<bool> EmailExistsAsync(string email, CancellationToken ct = default);
-    Task<User?> GetByRefreshTokenAsync(string refreshToken, CancellationToken ct = default);
-}
-
-public interface IArticleRepository : IRepository<Article>
-{
-    Task<int> GetLikeCountAsync(Guid articleId, CancellationToken ct = default);
-    Task<int> GetCommentCountAsync(Guid articleId, CancellationToken ct = default);
-}
-
 public interface IUnitOfWork
 {
     ISchoolRepository Schools { get; }
---
+++ backend/VidyaAI.Domain/Interfaces/ISchoolRepository.cs
@@ -0,0 +1,11 @@
+﻿using VidyaAI.Domain.Common;
+using VidyaAI.Domain.Entities;
+
+
+namespace VidyaAI.Domain.Interfaces
+{
+    public interface ISchoolRepository : IRepository<School>
+    {
+        Task<bool> EmailExistsAsync(string email, CancellationToken ct = default);
+    }
+}
