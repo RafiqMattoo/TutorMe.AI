62ab76472733a8955bb9523668ec54df5c2e6dc0
SumairMansoor <paulsumair197@gmail.com>
2026-07-28 11:44:07 +0530

    added VerificationEmailSentPage and VerificationSuccessPage

--- frontend/src/app/routes/publicRoutes.tsx
+++ frontend/src/app/routes/publicRoutes.tsx
@@ -1,10 +1,17 @@
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 import LoginPage from "../../features/auth/pages/LoginPage";
 import RegisterPage from "../../features/auth/pages/RegisterPage";
+import VerificationEmailSentPage from "../../features/auth/pages/VerificationEmailSentPage";
+import VerificationSuccessPage from "../../features/auth/pages/VerificationSuccessPage";
 
 export const publicRoutes = (
   <>
     <Route path="/login" element={<LoginPage />} />
     <Route path="/register" element={<RegisterPage />} />
+    <Route
+      path="/email-verification-sent"
+      element={<VerificationEmailSentPage />}
+    />
+    <Route path="/email-verified" element={<VerificationSuccessPage />} />
   </>
-)
+);
